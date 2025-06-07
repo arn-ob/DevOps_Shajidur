4a68f95f4a4fb3208b722340a2535a923eec34a4
Arnob <arnobxtreme@gmail.com>
2025-06-07 06:36:00 +0600

    update: health api add

--- weather_data_api/routes/api.js
+++ weather_data_api/routes/api.js
@@ -38,7 +38,7 @@ router.get('/forecast/:city', (req, res) => {
 
 router.get('/hello', (req, res) => {
 
-    console.log("API Requesting")
+    console.log("/hello - API Requesting")
 
     // fetch weather data from external API
     const apiKey = process.env.WEATHER_API_KEY;
@@ -52,7 +52,7 @@ router.get('/hello', (req, res) => {
             return response.json();
         })
         .then(data => {
-            
+
             // const dhakaTime = DateTime.fromFormat(data.location.localtime, 'yyMMddHHmm')
             const dt = DateTime.fromFormat(data.location.localtime, 'yyyy-MM-dd HH:mm', { zone: 'Asia/Dhaka' });
 
@@ -69,13 +69,37 @@ router.get('/hello', (req, res) => {
 
             });
 
-            console.log("API Response Sent")
+            console.log("/hello - API Response Sent")
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
             res.status(500).json({ error: 'Failed to fetch weather data' });
         });
 
+});
+
+router.get('/health', async (req, res) => {
+
+    console.log("/health - API Requesting")
+
+    try {
+        const apiKey = process.env.WEATHER_API_KEY;    
+        const apiUrl = `${process.env.WEATHER_API_URL}?key=${apiKey}&q=Dhaka&aqi=no`;
+
+        let response = await fetch(apiUrl);
+        response = await response.json();
+
+        if (response && response.location) {
+            console.log("/health - API Response Sent")
+            res.json({ status: 'ok', message: 'WeatherAPI is healthy' });
+        } else {
+            console.log("/health - API Response with Error")
+            res.status(500).json({ status: 'fail', message: 'Unexpected API response' });
+        }
+    } catch (error) {
+        console.log("/health - API Response with Error")
+        res.status(500).json({ status: 'fail', message: 'WeatherAPI is down or unreachable', error: error.message });
+    }
 });
 
 
