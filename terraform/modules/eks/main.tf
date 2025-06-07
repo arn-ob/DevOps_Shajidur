046fc370baf881cb15794da0a4f922c4112c51d9
Arnob <arnobxtreme@gmail.com>
2025-06-07 22:25:03 +0600

    update: tf eks improved

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,26 +1,26 @@
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# resource "aws_vpc" "eks_vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.cluster_name}-vpc"
-  }
-}
+#   tags = {
+#     Name = "${var.cluster_name}-vpc"
+#   }
+# }
 
-data "aws_availability_zones" "available" {}
+# data "aws_availability_zones" "available" {}
 
-resource "aws_subnet" "eks_subnet" {
-  count             = 2
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
+# resource "aws_subnet" "eks_subnet" {
+#   count             = 2
+#   vpc_id            = aws_vpc.eks_vpc.id
+#   cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "${var.cluster_name}-subnet-${count.index}"
-  }
-}
+#   tags = {
+#     Name = "${var.cluster_name}-subnet-${count.index}"
+#   }
+# }
 
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-cluster-role"
@@ -47,7 +47,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
+    subnet_ids = var.private_subnet_ids
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
@@ -84,13 +84,15 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
 }
 
 resource "aws_eks_node_group" "node_group" {
+  
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+  subnet_ids      = var.private_subnet_ids
+  
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     max_size     = 3
     min_size     = 1
   }
