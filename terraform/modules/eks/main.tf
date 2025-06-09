dd073ef26daee56e71404c7895f162c4339f991e
Arnob <arnobxtreme@gmail.com>
2025-06-09 23:15:22 +0600

    update: module eks update

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -31,7 +31,9 @@ resource "aws_eks_node_group" "node_group" {
   depends_on = [
     aws_iam_role_policy_attachment.worker_node_policy,
     aws_iam_role_policy_attachment.cni_policy,
-    aws_iam_role_policy_attachment.ecr_policy
+    aws_iam_role_policy_attachment.ecr_policy,
+    aws_iam_role_policy_attachment.ebs_csi_driver_policy,
+    aws_iam_policy.ebs_csi_policy,
   ]
 
   tags = {
@@ -88,5 +90,40 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_iam_policy" "ebs_csi_policy" {
+  name        = "AmazonEBSCSIDriverPolicy"
+  description = "Policy for EBS CSI Driver"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:AttachVolume",
+          "ec2:CreateSnapshot",
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteTags",
+          "ec2:DeleteVolume",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DetachVolume",
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
